38fee3840a1d4208e76512397acb453a7e2527ae
bvidal <bvidal@ltutech.com>
2014-09-10 22:33:16 -0400

    Add a method to retrieve rows in a file
    Using the csv module we parse the input files and returns each row separately.
    
    The test make sure we retrieve the expected number of rows

--- books.py
+++ books.py
@@ -2,6 +2,7 @@
 outputs the list with command line options to sort or filter them.
 """
 
+import csv
 import os
 
 
@@ -10,3 +11,14 @@ def get_input_files():
     return [os.path.join(os.path.abspath(os.path.dirname(__file__)), "data",
                          file_name) for file_name in ["pipe", "slash", "csv"]]
 
+
+def rows_from_file(file_path, delimiter):
+    """Yields the row in a file after parsing using the given delimiter.
+
+    Args:
+        file_path -- absolute path to a file
+        delimiter -- the delimiter used in the file to separate book fields
+    """
+    with open(file_path, "r") as input_file:
+        for row in csv.reader(input_file, delimiter=delimiter):
+            yield row
--- books_test.py
+++ books_test.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from books import get_input_files
+from books import get_input_files, rows_from_file
 
 
 class BooksTest(unittest.TestCase):
@@ -10,6 +10,11 @@ class BooksTest(unittest.TestCase):
         for file_path in get_input_files():
             self.assertTrue(os.path.isfile(file_path))
 
+    def test_row_from_file(self):
+        for (file_path, nb_lines, delimiter) in zip(get_input_files(),
+                                                    [4, 3, 2], ["|", "/", ","]):
+            self.assertEquals(len(list(rows_from_file(file_path, delimiter))),
+                              nb_lines)
 
 if __name__ == "__main__":
     unittest.main()
