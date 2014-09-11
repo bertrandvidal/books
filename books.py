cafa4402f6c8c3f64fb616e0ebb334972fa58179
bvidal <bvidal@ltutech.com>
2014-09-10 23:26:42 -0400

    Create functions to create books from rows
    Each file has a different order of fields these simple methods are here
    to make it possible to create a book instance from any known file.

--- books.py
+++ books.py
@@ -2,7 +2,7 @@
 outputs the list with command line options to sort or filter them.
 """
 
-from collections import namedtuple
+from collections import namedtuple, deque
 import csv
 import os
 
@@ -32,4 +32,28 @@ def rows_from_file(file_path, delimiter):
         for row in csv.reader(input_file, delimiter=delimiter):
             yield [field.strip() for field in row]
 
+
+# We define a series of lambda functions to convert a row from a specific
+# input_file into a book. We use the '_make' classmethod of the
+# namedtuple to create an instance of book from an iterable.
+
+
+# Pipe file just need to create the book instance
+pipe_to_book = lambda b: book._make(b)
+
+
+# Slash file requires to rotate the fields to create a book
+def slash_to_book(book_fields):
+    """Returns a book instance given fields read from a slash book.
+
+    Arg:
+        book_fields -- the list of fields as read from the slash file
+    """
+    slash_book = deque(book_fields)
+    slash_book.rotate(-1)
+    return book._make(slash_book)
+
+
+# Csv file needs a complete reorder of the fields
+csv_to_book = lambda b: book._make([b[2], b[1], b[0], b[3]])
 
--- books_test.py
+++ books_test.py
@@ -1,7 +1,8 @@
 import os
 import unittest
 
-from books import get_input_files, rows_from_file
+from books import (get_input_files, rows_from_file, pipe_to_book, slash_to_book,
+    csv_to_book)
 
 
 class BooksTest(unittest.TestCase):
@@ -15,6 +16,27 @@ class BooksTest(unittest.TestCase):
                                                     [4, 3, 2], ["|", "/", ","]):
             self.assertEquals(len(list(rows_from_file(file_path, delimiter))),
                               nb_lines)
+
+    def _assert_book_convertion(self, book, fields):
+        self.assertEquals(book.first_name, fields[0])
+        self.assertEquals(book.last_name, fields[1])
+        self.assertEquals(book.title, fields[2])
+        self.assertEquals(book.publication_date, fields[3])
+
+    def test_pipe_convertion(self):
+        fields = ["Kent", "Beck", "Test-Driven Development", "2002"]
+        self._assert_book_convertion(pipe_to_book(fields), fields)
+
+    def test_slash_convertion(self):
+        slash_book = slash_to_book(["1993", "Steve", "McConnell",
+                                    "Code Complete"])
+        self._assert_book_convertion(slash_book, ["Steve", "McConnell",
+                                                  "Code Complete", "1993"])
+
+    def test_csv_convertion(self):
+        csv_book = csv_to_book(["Clean Code", "Martin", "Robert", "2008"])
+        self._assert_book_convertion(csv_book, ["Robert", "Martin",
+                                                "Clean Code", "2008"])
 
 if __name__ == "__main__":
     unittest.main()
